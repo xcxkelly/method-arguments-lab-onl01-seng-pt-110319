587334b72185106bb203eadca5640f3f98e3d979
Kelly Small <xcxkelly98@gmail.com>
2019-11-15 13:08:45 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -6,5 +6,5 @@ end
 
 # introduction_with_language
 def language(ruby)
-  puts "Hi, my name is Kelly and i am learning to program in #{ruby}."
+  puts "Hi, my name is kelly and i am learning to program in #{ruby}."
 end
