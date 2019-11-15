226a17036502a919ef2cb3d998463141ec238876
Kelly Small <xcxkelly98@gmail.com>
2019-11-15 13:17:19 +0000

    Done.

---
+++ lib/introduction.rb
@@ -0,0 +1,11 @@
+# lib/introduction.rb
+
+def introduction(kelly)
+puts "Hi, my name is #{kelly} and i am learning to program in ruby."
+end
+
+# lib/language.rb
+
+def language(ruby)
+  puts "Hi, my name is kelly and i am learning to program in #{ruby\n}."
+end
